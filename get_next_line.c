52d3ca00aaccc6df3a1e0cbc945f7fba8237abcb
Katarina Slotova <66918113+Katarina-Slotova@users.noreply.github.com>
2022-01-03 16:05:20 +0200

    Final version of GNL, bonuses included.
    Fixed memory leaks and small errors.

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,13 @@
 /*   By: kslotova <kslotova@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/09 08:48:09 by kslotova          #+#    #+#             */
-/*   Updated: 2021/12/22 15:07:50 by kslotova         ###   ########.fr       */
+/*   Updated: 2022/01/03 16:02:01 by kslotova         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*ft_leftover(char *str)
+static char	*ft_leftover(char *str)
 {
 	char	*saved_str;
 	int		i;
@@ -35,10 +35,11 @@ char	*ft_leftover(char *str)
 		j++;
 	}
 	saved_str[j] = '\0';
+	free(str);
 	return (saved_str);
 }
 
-char	*ft_completed_line(char *str)
+static char	*ft_completed_line(char *str)
 {
 	char	*str_newline;
 	int		i;
@@ -61,42 +62,51 @@ char	*ft_completed_line(char *str)
 	return (str_newline);
 }
 
-int	get_next_line(const int fd, char **line)
+static char	*ft_read_file(char **saved_strs, int fd, char *buff, int *res)
 {
-	char		*buff;
-	static char	*saved_strs[FD_MAX];
-	int			read_res;
-	
-	if (!saved_strs[fd])
-		saved_strs[fd] = "";
+	int		read_res;
+	char	*tmp;
+
 	read_res = 1;
-	if (!fd || !line || BUFF_SIZE <= 0 || fd < 0 || fd > FD_MAX)
-		return (-1);
-	buff = (char *)malloc(sizeof(char) * (BUFF_SIZE + 1));
-	if (!buff)
-		return (-1);
 	while (read_res > 0 && !ft_strchr(saved_strs[fd], '\n'))
 	{
 		read_res = read(fd, buff, BUFF_SIZE);
+		*res = read_res;
 		if (read_res == -1)
-		{
-			free(saved_strs[fd]);
-			return (-1);
-		}
+			return (NULL);
 		buff[read_res] = '\0';
-		saved_strs[fd] = ft_strjoin(saved_strs[fd], buff);
+		tmp = ft_strjoin(saved_strs[fd], buff);
+		free(saved_strs[fd]);
+		saved_strs[fd] = tmp;
 	}
-	free(buff);
-	*line = ft_completed_line(saved_strs[fd]);
-	saved_strs[fd] = ft_leftover(saved_strs[fd]);
- 	if (read_res == 0)
-		return (0);
-	return (1);
+	return (saved_strs[fd]);
 }
 
-//Hello, I am a line from a file.\nThis is the 
-//next
-//line.\n
-//This is the next next line.\n
-//This is one more line that is 
-//somewhat longer.\n
+int	get_next_line(const int fd, char **line)
+{
+	char		buff[BUFF_SIZE + 1];
+	static char	*saved_strs[FD_MAX];
+	int			res;
+
+	if (fd < 0 || fd > FD_MAX || !line || BUFF_SIZE <= 0)
+		return (-1);
+	if (!saved_strs[fd])
+	{
+		saved_strs[fd] = (char *)malloc(sizeof(char));
+		if (!saved_strs[fd])
+			return (-1);
+		saved_strs[fd][0] = '\0';
+	}		
+	saved_strs[fd] = ft_read_file(saved_strs, fd, buff, &res);
+	if (res == 0 && saved_strs[fd][0] == '\0')
+	{
+		free(saved_strs[fd]);
+		saved_strs[fd] = NULL;
+		return (0);
+	}
+	*line = ft_completed_line(saved_strs[fd]);
+	saved_strs[fd] = ft_leftover(saved_strs[fd]);
+	if (res < 0)
+		return (-1);
+	return (1);
+}
